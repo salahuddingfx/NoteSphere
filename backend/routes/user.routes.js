12d2fca1caeec8f437c2db8501c070026117f58a
salahuddingfx <salauddinkaderappy@gmail.com>
2026-04-22 20:21:41 +0600

    feat: implement user routes for profile management, avatar uploads, and note interactions

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getLeaderboard, updateProfile, uploadAvatarPhoto, getContributionStats, getMyNotes, toggleSaveNote, getSavedNotes } = require("../controllers/user.controller");
+const { getLeaderboard, updateProfile, uploadAvatarPhoto, getContributionStats, getMyNotes, toggleSaveNote, getSavedNotes, getUserProfile } = require("../controllers/user.controller");
 
 const { protect } = require("../middlewares/auth.middleware");
 const uploadAvatar = require("../middlewares/uploadAvatar");
@@ -7,7 +7,9 @@ const uploadAvatar = require("../middlewares/uploadAvatar");
 const router = express.Router();
 
 router.get("/leaderboard", getLeaderboard);
+router.get("/profile/:username", getUserProfile);
 router.patch("/profile", protect, updateProfile);
+
 router.post("/avatar", protect, uploadAvatar.single("avatar"), uploadAvatarPhoto);
 router.get("/stats", protect, getContributionStats);
 router.get("/notes", protect, getMyNotes);
